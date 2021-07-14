d446e46290c3beab518f9c7337f02c5c22c59faa
yuki yoshinari <seal0828.4477@gmail.com>
2021-07-14 21:30:27 +0900

    update

--- react/src/setupProxy.js
+++ react/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
   app.use(
-    '/api',
+    '/yoshinari/api',
     createProxyMiddleware({
       target: 'http://python-flask:5000',
       // changeOrigin: true,
