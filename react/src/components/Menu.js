91f43e042f8a4e7486bc89f673b5d18e9eb971ca
yuki yoshinari <seal0828.4477@gmail.com>
2021-11-01 22:13:54 +0900

    add emotion and storybook

--- react/src/components/Menu.js
+++ react/src/components/Menu.js
@@ -1,17 +1,24 @@
+
 import React from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import './Content.css'
 import Home from './Home.js'
+import {css, jsx} from '@emotion/react'
 import {Typography, Button} from "@material-ui/core";
 import {useState, useContext} from "react"
 import axios from "axios";
 
+const style = css`
+  background-color: #00ad5f;
+`
 function Feedback(props) {
 
     return (
         <Home>
-            <div className="content-wrapper">
+            <div css={style}>
                 <Link to='/is-it-sentence'>is it sentence</Link>
+            </div>
+            <div css={style}>
                 <Link to='/feedback'>SST-2 anno</Link>
             </div>
         </Home>
