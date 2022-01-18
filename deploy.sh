8b04cf90a5201e2aa8f08bf6b4f65cb2fcb8786d
seal-git <seal0828.4477@gmail.com>
2022-01-19 02:41:35 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GCEでdocker-composeコマンドが使えなかったため,それ用のdocker imageを使用
-docker stop sab saf sad sag
+docker stop sab saf sad sag doccano
 cd ~/sampleapp1
 git checkout production
 git fetch origin
